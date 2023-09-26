c0914710efa6aee7971db690b15c023c3db5a0ac
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-09-26 10:30:46 -0500

    RadioGroup con nombre de producto y SKU

--- packages/terminal/src/presentation/views/terminal.tsx
+++ packages/terminal/src/presentation/views/terminal.tsx
@@ -1,6 +1,82 @@
 import { useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { RadioGroup } from "@headlessui/react";
 import { Switch } from "@headlessui/react";
+
+const products = [
+  {
+    id: 1,
+    nombre: "Aspirin",
+    sku: "ASPIRIN-123",
+    stock: 100,
+  },
+  {
+    id: 2,
+    nombre: "Ibuprofen",
+    sku: "IBUPROFEN-456",
+    stock: 75,
+  },
+  {
+    id: 3,
+    nombre: "Antiseptic Ointment",
+    sku: "ANTISEPTIC-789",
+    stock: 50,
+  },
+  {
+    id: 4,
+    nombre: "Cough Syrup",
+    sku: "COUGH-321",
+    stock: 120,
+  },
+  {
+    id: 5,
+    nombre: "Bandages",
+    sku: "BANDAGES-654",
+    stock: 200,
+  },
+  {
+    id: 6,
+    nombre: "Antacid Tablets",
+    sku: "ANTACID-987",
+    stock: 90,
+  },
+  {
+    id: 7,
+    nombre: "First Aid Kit",
+    sku: "FIRSTAID-789",
+    stock: 15,
+  },
+  {
+    id: 8,
+    nombre: "Digital Thermometer",
+    sku: "THERMOMETER-567",
+    stock: 30,
+  },
+  {
+    id: 9,
+    nombre: "Eye Drops",
+    sku: "EYEDROPS-234",
+    stock: 60,
+  },
+  {
+    id: 10,
+    nombre: "Pain Relief Gel",
+    sku: "PAINRELIEF-456",
+    stock: 40,
+  },
+  {
+    id: 11,
+    nombre: "Cotton Balls",
+    sku: "COTTONBALLS-123",
+    stock: 150,
+  },
+  {
+    id: 12,
+    nombre: "Allergy Medication",
+    sku: "ALLERGY-789",
+    stock: 70,
+  },
+];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -8,6 +84,7 @@ function classNames(...classes: string[]) {
 
 export default function Terminal() {
   const [agreed, setAgreed] = useState(false);
+  const [selected, setSelected] = useState(products[0]);
 
   return (
     <div className="isolate bg-white px-6 py-4 sm:py-12 lg:px-8">
@@ -58,6 +135,85 @@ export default function Terminal() {
               Despachar Receta Electrónica
             </Switch.Label>
           </Switch.Group>
+
+          <div className="sm:col-span-2">
+            <RadioGroup
+              value={selected}
+              onChange={setSelected}
+              className="mt-4"
+            >
+              <RadioGroup.Label className="sr-only">
+                Seleccione un producto
+              </RadioGroup.Label>
+              <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                {products.map((item) => (
+                  <RadioGroup.Option
+                    key={item.id}
+                    value={item}
+                    disabled={!item.stock}
+                    className={({ active }) =>
+                      classNames(
+                        item.stock
+                          ? "cursor-pointer bg-white text-gray-900 shadow-sm"
+                          : "cursor-not-allowed bg-gray-50 text-gray-200",
+                        active ? "ring-2 ring-indigo-500" : "",
+                        "rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
+                      )
+                    }
+                  >
+                    {({ active, checked }) => (
+                      <>
+                        <RadioGroup.Label as="p">
+                          {item.nombre}
+                        </RadioGroup.Label>
+                        <RadioGroup.Description
+                          as="span"
+                          className={`inline ${
+                            checked ? "text-sky-100" : "text-gray-500"
+                          }`}
+                        >
+                          <span>SKU: {item.sku}</span>
+                        </RadioGroup.Description>
+                        {item.stock ? (
+                          <span
+                            className={classNames(
+                              active ? "border" : "border-2",
+                              checked
+                                ? "border-indigo-500"
+                                : "border-transparent",
+                              "pointer-events-none absolute -inset-px rounded-md"
+                            )}
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <span
+                            aria-hidden="true"
+                            className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
+                          >
+                            <svg
+                              className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                              viewBox="0 0 100 100"
+                              preserveAspectRatio="none"
+                              stroke="currentColor"
+                            >
+                              <line
+                                x1={0}
+                                y1={100}
+                                x2={100}
+                                y2={0}
+                                vectorEffect="non-scaling-stroke"
+                              />
+                            </svg>
+                          </span>
+                        )}
+                      </>
+                    )}
+                  </RadioGroup.Option>
+                ))}
+              </div>
+            </RadioGroup>
+          </div>
+
           <div>
             <div>
               <label
@@ -78,27 +234,10 @@ export default function Terminal() {
             </div>
             <div>
               <label
-                htmlFor="doc-identidad"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Doc. Identidad
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="doc-identidad"
-                  id="doc-identidad"
-                  autoComplete="cc"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
                 htmlFor="first-name"
                 className="block text-sm font-semibold leading-6 text-gray-900"
               >
-                Nombre
+                Paciente
               </label>
               <div className="mt-2.5">
                 <input
@@ -106,23 +245,6 @@ export default function Terminal() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="last-name"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Apellido
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -138,14 +260,12 @@ export default function Terminal() {
                 <textarea
                   name="message"
                   id="message"
-                  rows={4}
+                  rows={2}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   defaultValue={""}
                 />
               </div>
             </div>
-          </div>
-          <div className="sm:col-span-2">
             <div className="mt-6 flex items-center justify-end gap-x-6">
               <button
                 type="button"
