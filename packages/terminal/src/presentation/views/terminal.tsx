f5012624940cf84eda7c374782babbee51989aed
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-09-28 11:51:14 -0500

    Remover background demo y agregar input para búsqueda

--- packages/terminal/src/presentation/views/terminal.tsx
+++ packages/terminal/src/presentation/views/terminal.tsx
@@ -112,7 +112,7 @@ export default function Terminal() {
         }
         getProductoBodegaCollectionLazyQuery({
             variables: {
-                inputWhere: { bodega_id: { is: 2 }, producto: { nombre: { contains: "ina" } } },                
+                inputWhere: { bodega_id: { is: 2 }, producto: { nombre: { contains: "ina" } } },
                 inputOrder: { asc: "producto.nombre" },
             },
             onCompleted: (c: any) => {
@@ -123,50 +123,48 @@ export default function Terminal() {
     }, [selected]);
 
     return (
-        <div className="isolate bg-white px-6 py-4 sm:py-12 lg:px-8">
-            <div
-                className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-                aria-hidden="true"
-            >
-                <div
-                    className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-                    style={{
-                        clipPath:
-                            "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                    }}
-                />
-            </div>
+        <div className="isolate bg-white px-6 py-4 sm:py-12 lg:px-8">           
             <div className="mx-auto max-w-2xl text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Despacho de Recetas</h2>
             </div>
-            <form action="#" method="POST" className="mx-auto mt-8 max-w-full sm:mt-12">
-                <div className="grid grid-cols-1 gap-x-4 gap-y-2 md:grid-cols-2 lg:grid-cols-3">
-                    <Switch.Group as="div" className="flex gap-x-4 col-span-full">
-                        <div className="flex h-6 items-center">
-                            <Switch
-                                checked={agreed}
-                                onChange={setAgreed}
+            <div className="grid grid-cols-1 gap-x-4 gap-y-2 md:grid-cols-2 lg:grid-cols-3">
+                <Switch.Group as="div" className="flex gap-x-4 col-span-full">
+                    <div className="flex h-6 items-center">
+                        <Switch
+                            checked={agreed}
+                            onChange={setAgreed}
+                            className={classNames(
+                                agreed ? "bg-indigo-600" : "bg-gray-200",
+                                "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            )}
+                        >
+                            <span className="sr-only">Receta Eletrónica</span>
+                            <span
+                                aria-hidden="true"
                                 className={classNames(
-                                    agreed ? "bg-indigo-600" : "bg-gray-200",
-                                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    agreed ? "translate-x-3.5" : "translate-x-0",
+                                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
                                 )}
-                            >
-                                <span className="sr-only">Receta Eletrónica</span>
-                                <span
-                                    aria-hidden="true"
-                                    className={classNames(
-                                        agreed ? "translate-x-3.5" : "translate-x-0",
-                                        "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                                    )}
-                                />
-                            </Switch>
-                        </div>
-                        <Switch.Label className="text-sm leading-6 text-gray-600">Despachar Receta Electrónica</Switch.Label>
-                    </Switch.Group>
+                            />
+                        </Switch>
+                    </div>
+                    <Switch.Label className="text-sm leading-6 text-gray-600">Despachar Receta Electrónica</Switch.Label>
+                </Switch.Group>
 
-                    <div className="lg:col-span-2">
-                        <RadioGroup value={selected} onChange={setSelected} className="mt-4">
-                            <RadioGroup.Label>Seleccione un producto</RadioGroup.Label>
+                <div className="lg:col-span-2">
+                    <div className="border rounded px-2 py-2">
+                        <div className="my-2">
+                            <input
+                                id="producto-nombre"
+                                name="producto-nombre"
+                                type="text"
+                                autoComplete="producto-nombre"
+                                className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Digite nombre de producto o SKU para buscar"
+                            />
+                        </div>
+                        <RadioGroup value={selected} onChange={setSelected} className="">
+                            <RadioGroup.Label className="sr-only">Seleccione un producto</RadioGroup.Label>
                             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                                 {products.map((item) => (
                                     <RadioGroup.Option
@@ -229,9 +227,11 @@ export default function Terminal() {
                             </div>
                         </RadioGroup>
                     </div>
+                </div>
 
-                    {/* Formulario Receta */}
-                    <div className="lg:col-span-1">
+                {/* Formulario Receta */}
+                <div className="lg:col-span-1">
+                    <form action="#" method="POST" className="mx-auto mt-8 max-w-full sm:mt-12">
                         <div className="grid grid-cols-1 gap-x-2 sm:grid-cols-2">
                             <div>
                                 <label htmlFor="no-receta" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -359,9 +359,9 @@ export default function Terminal() {
                                 </button>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
